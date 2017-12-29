eb03ccc935ffaab6c536c367a50a2ab20000ebdd
Akhmad Muzanni Safi'i <akhmadmuzannisafii@gmail.com>
2017-12-29 20:32:39 +0700

    rename landing to index

--- FilkomNews/www/ajax.php
+++ FilkomNews/www/ajax.php
@@ -1,5 +1,28 @@
 <?php 
+
 	$url = file_get_contents('http://filkom.ub.ac.id/module/api/conf/get_berita/in');
 	$data = base64_decode($url);
+	$pos = 0;
+	$arrayPos = array();
+	$arrayClose = array();
+	while(strpos($data,'id', $pos) !== false && $pos < strlen($data)){
+		$posNow = strpos($data,'<img', ++$pos);
+		if ($posNow !== false){
+			// echo $posNow."-";
+			$pos = $posNow;
+			array_push($arrayPos, $pos);
+		}
+	}
+	foreach ($arrayPos as $arrayImg) {
+		$posNow = strpos($data,'>', ++$arrayImg);
+		if ($posNow !== false){
+			array_push($arrayClose, $posNow);
+		}
+	}
+	for ($i=0; $i < $arrayPos; $i++) { 
+		echo substr($data, $arrayPos[$i], $arrayClose[$i]);
+	}
+	print_r($arrayPos);
+	print_r($arrayClose);
 	echo $data;
  ?>
